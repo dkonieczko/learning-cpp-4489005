accf4f1ce842b94483188591d7f809c63d2499c9
DK <danielkonieczko@hotmail.com>
2026-06-04 12:58:56 +0000

    My hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Daniel Konieczko
+
+#include <iostream>
+
+int main () {
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << std::
+    return 0;
+}
