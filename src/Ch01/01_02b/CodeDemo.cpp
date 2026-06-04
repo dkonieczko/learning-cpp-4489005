77f18e10f6c274ad6d684097ff72cd6b5435b7e5
DK <danielkonieczko@hotmail.com>
2026-06-04 13:15:56 +0000

    updated code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -5,8 +5,10 @@
 #include <iostream>
 
 int main () {
+    std::cout << std::endl << std::endl;
+    
     std::cout << "Hello World!" << std::endl;
 
-    std::cout << std::
+    std::cout << std::endl << std::endl;
     return 0;
 }
