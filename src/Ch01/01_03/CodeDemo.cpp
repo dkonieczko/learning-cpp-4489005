a687455c8ba4a006e3128384b121ba6810369a51
DK <danielkonieczko@hotmail.com>
2026-06-04 13:27:59 +0000

    New Code

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,14 +1,15 @@
 // Learning C++ 
 // Challenge 01_03
-// Console Interaction, by Eduardo Corpeño 
+// Code by Dan Konieczko
 
 #include <iostream>
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string name;
+    std::cout << "What is your name? ";
+    std::cin >> name;
+    std::cout << "Hi " << name << " how are you?" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
