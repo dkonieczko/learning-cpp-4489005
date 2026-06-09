c9f2d188fe464fc2c8f89448d84ed8cbd474e591
DK <danielkonieczko@hotmail.com>
2026-06-09 13:42:37 +0000

    updated

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -1,11 +1,23 @@
 // Learning C++ 
 // Exercise 02_10
-// Type Casting, by Eduardo Corpeño 
+// Type Casting, by Dan Konieczko 
 
 #include <iostream>
 #include <cstdint>
 
 int main(){
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+
+    std::cout << "float: " << flt << std::endl;
+    std::cout << "int32: " << sgn << std::endl;
+    std::cout << "uint32: " << unsgn << std::endl;
+
     
     std::cout << std::endl << std::endl;
     return (0);
--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -1,12 +1,34 @@
 // Learning C++ 
 // Exercise 02_11
-// Type Casting Examples, by Eduardo Corpeño 
+// Type Casting Examples, by Dan Konieczko 
 
 #include <iostream>
 #include <cstdint>
 
 int main(){
+    int fahrenheit = 99;
+    int celsius;
+
+    celsius = ((float)5 / 9.0) * (fahrenheit - 32);
+
+    std::cout << std::endl;
+    std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "celsius   : " << celsius << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << std::endl;
+    std::cout << "Float          : " << weight  << std::endl;
+    std::cout << "Integer part   : " << (int) weight  << std::endl;
+    std::cout << "Fractional part: " << (int)((weight - (int)weight) * 10000)  << std::endl;
     
+    double weight1 = 10.99;
+
+    std::cout << std::endl;
+    std::cout << "Double         : " << weight1  << std::endl;
+    std::cout << "Integer part   : " << (int) weight1  << std::endl;
+    std::cout << "Fractional part: " << (int)((weight1 - (int)weight1) * 10000)  << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
