3ba62d0c6aa959c08ade9f4c8c1972ebdc0d459b
DK <danielkonieczko@hotmail.com>
2026-06-09 11:53:23 +0000

    Strings

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -1,11 +1,25 @@
 // Learning C++ 
 // Exercise 02_09
-// Strings, by Eduardo Corpeño 
+// Strings, by Dan Konieczko 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    const size_t LENGTH1 = 25;
+
+    char aray_str1[LENGTH1] = "Hey guys! ";
+    char array_str2[] = "What's up?";
     
+    std::string std_str1 = "Hi everybody! ";
+    std::string std_str2 = "How's it going?";
+    
+    strncat(aray_str1, array_str2, LENGTH1);
+    std::cout << aray_str1 << std::endl;
+    std::cout << std_str1 + std_str2 << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
